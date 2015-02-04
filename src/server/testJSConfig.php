c4a8c5fd2e3cead3dc3ee8310c5285bb6f549904
Magamed <a1essar@yandex.ru>
2015-02-04 15:29:41 +0300

    Update file structure

--- src/server/testJSConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config = file_get_contents("../client/json/config.json");
-$config = json_decode($config, true);
-var_dump($config);
-$config['change-config-from-php'] = true;
-file_put_contents("../client/json/config.json", json_encode($config, JSON_PRETTY_PRINT));
-?>
