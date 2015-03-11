8299e1dba8f3af5549f80a17fcb6143be6eb9328
Magamed <a1essar@yandex.ru>
2015-03-11 20:01:52 +0300

    Fixed competition image link

--- src/client/scripts/js/views/competition.js
+++ src/client/scripts/js/views/competition.js
@@ -4,32 +4,32 @@
             {
                 "name": "User 1",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti minus nobis quaerat quod saepe sequi voluptas! Doloremque ducimus, ea eligendi fugiat harum iure iusto, magni obcaecati praesentium quod rem veniam!",
-                "image": "/images/stub.jpg"
+                "image": "../images/stub.jpg"
             },
             {
                 "name": "User 2",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti minus nobis quaerat quod saepe sequi voluptas! Doloremque ducimus, ea eligendi fugiat harum iure iusto, magni obcaecati praesentium quod rem veniam!",
-                "image": "/images/stub.jpg"
+                "image": "../images/stub.jpg"
             },
             {
                 "name": "User 3",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti minus nobis quaerat quod saepe sequi voluptas! Doloremque ducimus, ea eligendi fugiat harum iure iusto, magni obcaecati praesentium quod rem veniam!",
-                "image": "/images/stub.jpg"
+                "image": "../images/stub.jpg"
             },
             {
                 "name": "User 4",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti minus nobis quaerat quod saepe sequi voluptas! Doloremque ducimus, ea eligendi fugiat harum iure iusto, magni obcaecati praesentium quod rem veniam!",
-                "image": "/images/stub.jpg"
+                "image": "../images/stub.jpg"
             },
             {
                 "name": "User 5",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti minus nobis quaerat quod saepe sequi voluptas! Doloremque ducimus, ea eligendi fugiat harum iure iusto, magni obcaecati praesentium quod rem veniam!",
-                "image": "/images/stub.jpg"
+                "image": "../images/stub.jpg"
             },
             {
                 "name": "User 6",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti minus nobis quaerat quod saepe sequi voluptas! Doloremque ducimus, ea eligendi fugiat harum iure iusto, magni obcaecati praesentium quod rem veniam!",
-                "image": "/images/stub.jpg"
+                "image": "../images/stub.jpg"
             }
         ]
     };
